168431d8974240e26cb471850572e3989e2b6db9
prathamesh-patil-5090 <122prathamesh6002@sjcem.edu.in>
2024-12-12 22:36:43 +0530

    Completed till 56

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import NavBar from './components/Navbar';
+import { BrowserRouter as Router, Link, Routes, Route  } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook!</h1>
-    </div>
+    <>
+      <div className="App">
+        <NavBar /> {/* Add NavBar component */}
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </Router>
+      </div>
+    </>
   );
 }
 
