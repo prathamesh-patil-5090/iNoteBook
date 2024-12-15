113c8e035f44d212d5736d23e4bf47b3ed0b1246
prathamesh-patil-5090 <122prathamesh6002@sjcem.edu.in>
2024-12-15 22:58:23 +0530

    Updated SignUP

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -23,6 +23,7 @@ const Signup = () => {
   };
 
   const validateEmail = (email) => {
+    // eslint-disable-next-line no-useless-escape
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return regex.test(email);
   };
