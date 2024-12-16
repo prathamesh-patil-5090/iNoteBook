2d703b3d183280a149dfb974f5c2127269c112ec
Prathamesh Patil <134536755+prathamesh-patil-5090@users.noreply.github.com>
2024-12-16 21:40:36 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,10 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection string
+
 // const mongoDBURL = "mongodb://127.0.0.1:27017/iNotebookDatabase?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.4";
 const mongoDBURL = "YOUR_MONGODB_OR_MONGODB_ATLAS_CONNECTION_STRING";
-
+// You can use the above one too!
 // Connect to MongoDB
 mongoose.connect(mongoDBURL, {
     useNewUrlParser: true,
